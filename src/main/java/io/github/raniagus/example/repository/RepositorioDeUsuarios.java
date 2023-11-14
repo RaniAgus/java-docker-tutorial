2c878695c92a531e1387bec02fd9625e3286cb60
Agustin Ranieri <aguseranieri@gmail.com>
2023-11-14 09:36:18 -0300

    Remove implements

--- src/main/java/io/github/raniagus/example/repository/RepositorioDeUsuarios.java
+++ src/main/java/io/github/raniagus/example/repository/RepositorioDeUsuarios.java
@@ -1,10 +1,9 @@
 package io.github.raniagus.example.repository;
 
-import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import io.github.raniagus.example.model.Usuario;
 import java.util.Optional;
 
-public class RepositorioDeUsuarios extends Repositorio<Usuario> implements WithSimplePersistenceUnit {
+public class RepositorioDeUsuarios extends Repositorio<Usuario> {
   public static final RepositorioDeUsuarios INSTANCE = new RepositorioDeUsuarios();
 
   public Optional<Usuario> buscarPorEmail(String email) {
