3285da43bca8914a490351a218f2c723261b0c43
Agustin Ranieri <aguseranieri@gmail.com>
2023-11-14 09:21:17 -0300

    Add more config parameters

--- src/main/java/io/github/raniagus/example/Application.java
+++ src/main/java/io/github/raniagus/example/Application.java
@@ -4,11 +4,11 @@ import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.resolve.DirectoryCodeResolver;
 import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
+import io.github.raniagus.example.bootstrap.Bootstrap;
 import io.github.raniagus.example.controller.Controller;
 import io.github.raniagus.example.controller.ErrorController;
 import io.github.raniagus.example.controller.HomeController;
 import io.github.raniagus.example.controller.LoginController;
-import io.github.raniagus.example.helpers.Environment;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import io.javalin.rendering.JavalinRenderer;
@@ -18,8 +18,13 @@ import java.nio.file.Path;
 import java.time.LocalDate;
 
 public class Application {
+  public static final Config config = Config.create();
+
   public static void main(String[] args) {
     startDatabaseConnection();
+    if (config.databaseHbm2ddlAuto().equals("create-drop")) {
+      new Bootstrap().run();
+    }
     startServer(
         HomeController.INSTANCE,
         LoginController.INSTANCE,
@@ -28,25 +33,13 @@ public class Application {
   }
 
   public static void startDatabaseConnection() {
-    if (Environment.isDevelopment()) {
-      WithSimplePersistenceUnit.configure(properties -> properties
-              .set("hibernate.connection.url", Environment.getVariableOrDefault("DATABASE_URL", "jdbc:postgresql://localhost:5432/example"))
-              .set("hibernate.connection.username", Environment.getVariableOrDefault("DATABASE_USERNAME", "postgres"))
-              .set("hibernate.connection.password", Environment.getVariableOrDefault("DATABASE_PASSWORD", "postgres"))
-              .set("hibernate.hbm2ddl.auto", "update"));
-    } else {
-      WithSimplePersistenceUnit.configure(properties -> properties
-        .set("hibernate.connection.url", Environment.getVariable("DATABASE_URL"))
-        .set("hibernate.connection.username", Environment.getVariable("DATABASE_USERNAME"))
-        .set("hibernate.connection.password", Environment.getVariable("DATABASE_PASSWORD"))
-        .set("hibernate.hbm2ddl.auto", "validate"));
-    }
+    WithSimplePersistenceUnit.configure(properties -> properties.putAll(config.toProperties()));
     WithSimplePersistenceUnit.dispose();
   }
 
   public static void startServer(Controller... controllers) {
     JavalinValidation.register(LocalDate.class, LocalDate::parse);
-    JavalinRenderer.register(new JavalinJte(createTemplateEngine(), ctx -> Environment.isDevelopment()), ".jte");
+    JavalinRenderer.register(new JavalinJte(createTemplateEngine(), ctx -> config.isDevelopment()), ".jte");
     var app = Javalin.create(config -> {
       config.staticFiles.add("public", Location.EXTERNAL);
       config.accessManager(LoginController.INSTANCE);
@@ -59,18 +52,20 @@ public class Application {
   }
 
   private static TemplateEngine createTemplateEngine() {
-    if (Environment.isDevelopment()) {
-      var path = Path.of("src","main", "jte");
-      if (!path.toFile().isDirectory()) {
-        throw new IllegalStateException("src/main/jte directory does not exist");
-      }
-      return TemplateEngine.create(new DirectoryCodeResolver(path), ContentType.Html);
+    if (config.isDevelopment()) {
+      return TemplateEngine.create(new DirectoryCodeResolver(dirPath("src", "main", "jte")), ContentType.Html);
     } else {
-      var path = Path.of("jte-classes");
-      if (!path.toFile().isDirectory()) {
-        throw new IllegalStateException("jte-classes directory does not exist");
-      }
-      return TemplateEngine.createPrecompiled(path, ContentType.Html);
+      return TemplateEngine.createPrecompiled(dirPath("jte-classes"), ContentType.Html);
     }
+  }
+
+  private static Path dirPath(String first, String... more) {
+    var path = Path.of(first, more);
+    if (!path.toFile().isDirectory()) {
+      throw new IllegalStateException(
+          String.join("/", first, String.join("/", more)) + " directory does not exist"
+      );
+    }
+    return path;
   }
 }
---
+++ src/main/java/io/github/raniagus/example/Config.java
@@ -0,0 +1,67 @@
+package io.github.raniagus.example;
+
+import java.util.Objects;
+import java.util.Properties;
+
+public record Config (
+    boolean isDevelopment,
+    String databaseUrl,
+    String databaseUsername,
+    String databasePassword,
+    String databaseDriver,
+    String databaseDialect,
+    String databaseShowSql,
+    String databaseHbm2ddlAuto
+) {
+  public Config {
+    Objects.requireNonNull(databaseUrl, "databaseUrl is required");
+    Objects.requireNonNull(databaseUsername, "databaseUsername is required");
+    Objects.requireNonNull(databasePassword, "databasePassword is required");
+    Objects.requireNonNull(databaseDriver, "databaseDriver is required");
+    Objects.requireNonNull(databaseDialect, "databaseDialect is required");
+    Objects.requireNonNull(databaseShowSql, "databaseShowSql is required");
+    Objects.requireNonNull(databaseHbm2ddlAuto, "databaseHbm2ddlAuto is required");
+  }
+
+  public static Config create() {
+    return System.getenv().containsKey("PRODUCTION") ? createProd() : createDev();
+  }
+
+  private static Config createProd() {
+    return new Config(
+        false,
+        System.getenv("DATABASE_URL"),
+        System.getenv("DATABASE_USERNAME"),
+        System.getenv("DATABASE_PASSWORD"),
+        System.getenv("DATABASE_DRIVER"),
+        System.getenv("DATABASE_DIALECT"),
+        System.getenv("DATABASE_SHOW_SQL"),
+        System.getenv("DATABASE_HBM2DDL_AUTO")
+    );
+  }
+
+  private static Config createDev() {
+    return new Config(
+        true,
+        System.getenv().getOrDefault("DATABASE_URL", "jdbc:postgresql://localhost:5432/example"),
+        System.getenv().getOrDefault("DATABASE_USERNAME", "postgres"),
+        System.getenv().getOrDefault("DATABASE_PASSWORD", "postgres"),
+        System.getenv().getOrDefault("DATABASE_DRIVER", "org.postgresql.Driver"),
+        System.getenv().getOrDefault("DATABASE_DIALECT", "org.hibernate.dialect.PostgresPlusDialect"),
+        System.getenv().getOrDefault("DATABASE_SHOW_SQL", "true"),
+        System.getenv().getOrDefault("DATABASE_HBM2DDL_AUTO", "create-drop")
+    );
+  }
+
+  public Properties toProperties() {
+    var properties = new Properties();
+    properties.setProperty("hibernate.connection.url", databaseUrl);
+    properties.setProperty("hibernate.connection.username", databaseUsername);
+    properties.setProperty("hibernate.connection.password", databasePassword);
+    properties.setProperty("hibernate.connection.driver_class", databaseDriver);
+    properties.setProperty("hibernate.dialect", databaseDialect);
+    properties.setProperty("hibernate.show_sql", databaseShowSql);
+    properties.setProperty("hibernate.hbm2ddl.auto", databaseHbm2ddlAuto);
+    return properties;
+  }
+}
