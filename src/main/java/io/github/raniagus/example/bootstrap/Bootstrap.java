377e3dbac0975877916d02ca09add03e3927518f
Agustin Ranieri <aguseranieri@gmail.com>
2023-11-24 15:26:54 -0300

    Extract InputStream from CsvReader

--- src/main/java/io/github/raniagus/example/bootstrap/Bootstrap.java
+++ src/main/java/io/github/raniagus/example/bootstrap/Bootstrap.java
@@ -6,6 +6,10 @@ import io.github.raniagus.example.repository.RepositorioDeUsuarios;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+
 public class Bootstrap implements Runnable, WithSimplePersistenceUnit {
   private static final Logger log = LoggerFactory.getLogger(Bootstrap.class);
 
@@ -17,11 +21,10 @@ public class Bootstrap implements Runnable, WithSimplePersistenceUnit {
   @Override
   public void run() {
     log.info("Iniciando reinicio de base de datos...");
-    try {
-      var users = new CsvReader<>(UserDto.class, "data/users.csv")
-          .readAll().stream()
-          .map(UserDto::toEntity)
-          .toList();
+    var path = Paths.get("data", "users.csv").toAbsolutePath().toString();
+
+    try (var reader = new CsvReader<>(UserDto.class, new FileInputStream(path))){
+      var users = reader.readAll().stream().map(UserDto::toEntity).toList();
 
       withTransaction(() -> {
         RepositorioDeUsuarios.INSTANCE.eliminarTodos();
@@ -29,8 +32,8 @@ public class Bootstrap implements Runnable, WithSimplePersistenceUnit {
       });
 
       users.forEach(user -> log.info("Usuario insertado: {}", user));
-    } catch (Exception e) {
-      log.error("Error al insertar datos de prueba", e);
+    } catch (FileNotFoundException e) {
+      log.error("No se pudo encontrar el archivo {}", path, e);
     }
   }
 }
--- src/main/java/io/github/raniagus/example/bootstrap/CsvReader.java
+++ src/main/java/io/github/raniagus/example/bootstrap/CsvReader.java
@@ -2,29 +2,36 @@ package io.github.raniagus.example.bootstrap;
 
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.util.List;
 
-public class CsvReader<T> {
+public class CsvReader<T> implements AutoCloseable {
   private final CsvMapper mapper;
   private final Class<T> clazz;
-  private final String filename;
+  private final InputStream inputStream;
 
-  public CsvReader(Class<T> clazz, String filename) {
+  public CsvReader(Class<T> clazz, InputStream inputStream) {
     this.mapper = new CsvMapper();
     this.clazz = clazz;
-    this.filename = filename;
+    this.inputStream = inputStream;
   }
 
   @SuppressWarnings("unchecked")
   public List<T> readAll() {
     var schema = mapper.schemaFor(clazz).withHeader();
-    try (var inputStream = new FileInputStream(filename)) {
-      try (var reader = mapper.readerFor(clazz).with(schema).readValues(inputStream)) {
-        return (List<T>) reader.readAll();
-      }
+    try (var reader = mapper.readerFor(clazz).with(schema).readValues(inputStream)) {
+      return (List<T>) reader.readAll();
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  @Override
+  public void close() {
+    try {
+      inputStream.close();
     } catch (IOException e) {
       throw new UncheckedIOException(e);
     }
