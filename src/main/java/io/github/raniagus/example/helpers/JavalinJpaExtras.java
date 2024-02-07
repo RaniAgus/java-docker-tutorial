7153c86fda235062a84f044bb6f628a87700151e
Agustin Ranieri <aguseranieri@gmail.com>
2024-02-07 17:23:25 -0300

    Update JavalinJpaExtras.java

--- src/main/java/io/github/raniagus/example/helpers/JavalinJpaExtras.java
+++ src/main/java/io/github/raniagus/example/helpers/JavalinJpaExtras.java
@@ -3,14 +3,11 @@ package io.github.raniagus.example.helpers;
 import io.github.flbulgarelli.jpa.extras.perthread.PerThreadEntityManagerProperties;
 import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import io.javalin.config.JavalinConfig;
-import io.javalin.http.Context;
-import io.javalin.plugin.ContextPlugin;
+import io.javalin.plugin.Plugin;
 import java.util.function.Consumer;
-import org.jetbrains.annotations.NotNull;
 
 public class JavalinJpaExtras
-      extends ContextPlugin<Consumer<PerThreadEntityManagerProperties>, JavalinJpaExtras>
-      implements WithSimplePersistenceUnit {
+      extends Plugin<Consumer<PerThreadEntityManagerProperties>> {
   public JavalinJpaExtras(Consumer<PerThreadEntityManagerProperties> pluginConfig) {
     this.pluginConfig = pluginConfig;
   }
@@ -19,10 +16,5 @@ public class JavalinJpaExtras
   public void onInitialize(JavalinConfig config) {
     WithSimplePersistenceUnit.configure(pluginConfig);
     config.router.mount(router -> router.after(ctx -> WithSimplePersistenceUnit.dispose()));
-  }
-
-  @Override
-  public JavalinJpaExtras createExtension(@NotNull Context context) {
-    return this;
   }
 }
